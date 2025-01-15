ce9b9ed5e057fbeca295822dc93f66e2b66729d2
jmcho2010 <projm2786@gmail.com>
2025-01-15 15:38:05 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+    data(){
+      return{
+        message: '오 이게 vue구나.'
+      };
+    }
+  };
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style>
+
+</style>
