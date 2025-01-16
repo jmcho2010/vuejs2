b6f979a9842b0b1dfbca749ec37527324c77af87
jmcho2010 <projm2786@gmail.com>
2025-01-16 09:32:05 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Food</h1>
+    <props-exam food-name="Apples"/>
+    <props-exam food-name="Chicken"/>
+    <props-exam food-name="pizza"/>
+    <props-exam food-name="pasta"/>
+
+</template>
+
+<script></script>
+
+<style>
+    #app > div{
+        border:dashed black 1px;
+        display: inline-block;
+        width: 120px;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@
 import { createApp } from 'vue'
 import App from './app-todoList.vue' // app 컴포넌트 인식
 //import ItemList from './components/ItemList.vue'//ItemList 정보 
-
+import PropsExam from './components/PropsExam.vue';
 
 const app = createApp(App)
 
 //app.component('item-list', ItemList);
-app.component('props-exam', PropsExam)
+app.component('props-exam', PropsExam);
 
 
 app.mount('#app')
