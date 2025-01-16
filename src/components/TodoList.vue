2bb8b692a22003b994157a3fa63547262f332e24
jmcho2010 <projm2786@gmail.com>
2025-01-16 09:07:31 +0900

    test

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,58 @@
 <template>
 
+    <div class="todo-container">
+        <!-- 새로운 할일 입력받기-->
+        <div class="input-group">
+            <input type="text" v-model="newTodo" 
+                   @keyup.enter="addTodo" 
+                   placeholder="할 일 입력"
+                   class="todo-input">
+            <button @click="addTodo" class="add-btn">추가</button>
+        </div>
+        <div class="filter-group">
+            <!-- 아래의 버튼은 다음의 규칙으로 설계
+            반복 키워드를 활용하여 filters 변수의 내용을 하나하나씩 반복
+             -> 추후 기능 추가시 건드릴게 적음.
+            :key="filter" //Vue의 리스트 렌더링을 위한 고유 키 부여.
+            클릭시 현재의 필터값을 변경후 클래스도 active를 추가
+            -->
+            <button 
+                v-for="filter in filters"  
+                :key="filter"
+                @click="currentFilter = filter" 
+                :class="['filter-btn', { active: currentFilter === filter}]"
+            >
+            {{ filter }}
+            </button>
+        </div>
+
+        <!-- 할 일 목록-->
+        <ul class="todo-list">
+            <li
+                v-for="todo in filteredTodos"
+                :key="todo.id"
+                :class="['todo-item', { completed: todo.completed }]"
+            >
+            <!-- 이 checkbox는 완료여부를 체크하는데 completed 속성과 양방향으로 바인딩을 체크-->
+            <input type="checkbox"
+                v-model="todo.completed"
+                class="todo-checkbox"
+            >
+            <span class="todo-text">{{ todo.text }}</span> 
+            <button 
+                @click="removeTodo(todo.id)"  
+                class="delete-btn"
+            >❌
+            </button>
+            </li>
+        </ul>
+
+        <!-- 남은 할 일 개수 표시 -->
+        <div class="todo-stats">
+            남은 할 일: {{ remaininTodos }} 개  
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -33,11 +86,35 @@ import {ref, computed} from 'vue'
                  // -> 선택된 id를 제외한 새 배열 생성
             }
 
-            // 필터링된 할일 목록
+ 
+            // 할일 목록 필터링 처리
             const filteredTodos = computed(() =>{
-                switch (currentFilter.value)
+
+                switch (currentFilter.value){
+                    case '진행중': // 미완료 목록만 view
+                        return todos.value.filter(todo => !todo.completed);
+                    case '완료': // 완료 목록만 보여줄때
+                        return todos.value.filter(todo => todo.completed);
+                    default:
+                        return todos.value
+                }
+            })
+            // 남은 할일 개수 계산(계산된 속성)
+            const remaininTodos = computed(() =>{
+                return todos.value.filter(todo => !todo.completed).length
             })
 
+
+            return{
+                todos,
+                newTodo,
+                currentFilter,
+                filters,
+                addTodo,
+                removeTodo,
+                filteredTodos,
+                remaininTodos
+            }
         }
     }
 
