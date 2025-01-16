4153eec69fac2b5ee1f42ea262f6603037bd3060
jmcho2010 <projm2786@gmail.com>
2025-01-16 16:37:52 +0900

    test

---
+++ src/appQuiz.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div>
+        <button @click="showModal('AlertModal', { message: '안녕하세요!' })">
+            알림 모달
+        </button>
+
+        <transition name="modal">
+            <component
+                v-if="currentModal"
+                :is="currentModal"
+                v-bind="modalProps"
+                @close="closeModal"
+                @confirm="handleConfirm"
+            ></component>
+        </transition>
+    </div>
+
+</template>
+
+<script>
+import AlertModal from './quiz1compo/AlertModal.vue'
+
+export default{
+    components:{
+        AlertModal
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+
+</script>
+
+<style>
+.modal-enter-active, .modal-leave-active {
+  transition: opacity 0.3s;
+}
+.modal-enter, .modal-leave-to {
+  opacity: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './app.vue' // app 컴포넌트 인식
+//import App from './app.vue' // app 컴포넌트 인식
+import app from './appQuiz.vue';
 //import ItemList from './components/ItemList.vue'//ItemList 정보 
 //import PropsExam from './components/PropsExam.vue';
-import SlotComp from './components/SlotComp.vue';
+// import SlotComp from './components/SlotComp.vue';
 
 
 // import CompOne from './components/CompOne.vue'
@@ -14,7 +15,7 @@ app.component('slot-comp', SlotComp);
 
 //app.component('item-list', ItemList);
 //app.component('props-exam', PropsExam);
-// app.component('comp-one', CompOne)
-// app.component('comp-two', CompTwo)
+app.component('comp-one', CompOne)
+app.component('comp-two', CompTwo)
 
 app.mount('#app')
