952cff019efa417cae46b7d2fd0149b66a29b541
jmcho2010 <projm2786@gmail.com>
2025-01-16 14:21:10 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,39 @@
 <template>
-    <h1>Food</h1>
-    <div class="wrapper">
-        <props-exam
-         v-for="i in foods"
-         :key="i.name"
-         :food-name="i.name"
-         :food-desc="i.desc"
-         :is-favorite="i.favorite"
-         @toggle-favorite="receiveEmit"/>
+    <div>
+      <h3>Global Components</h3>
+      <p>App.vue</p>
+      <p>The CompOne.vue component is used inside both App.vue and CompTwo.vue.</p>
+      <comp-one /> <br>
+      <comp-two />
     </div>
-    <!-- 
-    아래와 같은 방식으로 웹 서비스를 제공하는것은
-    서비스 유지보수시 매우 힘듬.
-    데이터를 좀더 쉽고 빠르게 그리고 개발자가 손을 덜쓰는쪽으로
-    방향을 수정할 필요가 있음.
-    <div class="wrapper">
-        <props-exam 
-    food-name="Apples"
-    food-desc="사과는 사과 나무에서 자라요"
-    v-bind:is-favorite="true"
-    />
-    <props-exam 
-    food-name="Chicken"
-    food-desc="치킨은 치킨나무에서 자라요"
-    v-bind:is-favorite="true"
-    />
-    <props-exam 
-    food-name="pizza"
-    v-bind:is-favorite="false"
-    />
-    <props-exam 
-    food-name="pasta"
-    food-desc="파스타는 파스타나무에서 자라요"
-    v-bind:is-favorite="false"
-    />
-    </div> -->
-</template>
+  </template>
+  
+  <script>
+  // 컴포넌트 지역화
+  // 원하는 컴포넌트에 선언함으로써 지역화를 진행.
+    import CompOne from './components/CompOne.vue'
 
-<script>
-export default{
-    data(){
-        return{
-            foods: [
-                {name: '사과',
-                 desc: '사과는 사과나무에서 열려요',
-                 favorite: true   
-                },
-                {name: '피자',
-                 desc: '피자는 피자나무에서 열려요',
-                 favorite: true   
-                }
-            ]
-        };
-    },
-    methods:{
-        // emit을 활용할경우
-        // 자식컴포넌트에는 같은 메서드를
-        // 만들이유가 없기때문에 재사용성 상승의
-        // 효과를 누릴수 있음.
-        receiveEmit(food){
-            alert("선택한 항목 : " + food);
+    export default{
+        components:{
+            'comp-one' : CompOne
         }
     }
-}
-
-</script>
-
-<style>
-    .wrapper{
-        display: flex;
-        flex-wrap:wrap;
+  </script>
+  
+  <style>
+    p {
+      width: 200px;
     }
-
-    .wrapper > div{
-        border:dashed black 1px;
-        flex-basis: 150px;
-        margin: 10px;
-        padding: 10px;
-        background-color: lightgreen;
+    #app div {
+      border: dashed black 1px;
+      margin: 10px;
+      padding: 10px;
+      display: inline-block;
     }
-</style>
+    .compOneDiv {
+      background-color: lightgreen;
+    }
+    .compTwoDiv {
+      background-color: lightcoral;
+    }
+  </style>   
---
+++ src/app-props.vue
@@ -0,0 +1,82 @@
+<template>
+    <h1>Food</h1>
+    <div class="wrapper">
+        <props-exam
+         v-for="i in foods"
+         :key="i.name"
+         :food-name="i.name"
+         :food-desc="i.desc"
+         :is-favorite="i.favorite"
+         @toggle-favorite="receiveEmit"/>
+    </div>
+    <!-- 
+    아래와 같은 방식으로 웹 서비스를 제공하는것은
+    서비스 유지보수시 매우 힘듬.
+    데이터를 좀더 쉽고 빠르게 그리고 개발자가 손을 덜쓰는쪽으로
+    방향을 수정할 필요가 있음.
+    <div class="wrapper">
+        <props-exam 
+    food-name="Apples"
+    food-desc="사과는 사과 나무에서 자라요"
+    v-bind:is-favorite="true"
+    />
+    <props-exam 
+    food-name="Chicken"
+    food-desc="치킨은 치킨나무에서 자라요"
+    v-bind:is-favorite="true"
+    />
+    <props-exam 
+    food-name="pizza"
+    v-bind:is-favorite="false"
+    />
+    <props-exam 
+    food-name="pasta"
+    food-desc="파스타는 파스타나무에서 자라요"
+    v-bind:is-favorite="false"
+    />
+    </div> -->
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            foods: [
+                {name: '사과',
+                 desc: '사과는 사과나무에서 열려요',
+                 favorite: true   
+                },
+                {name: '피자',
+                 desc: '피자는 피자나무에서 열려요',
+                 favorite: true   
+                }
+            ]
+        };
+    },
+    methods:{
+        // emit을 활용할경우
+        // 자식컴포넌트에는 같은 메서드를
+        // 만들이유가 없기때문에 재사용성 상승의
+        // 효과를 누릴수 있음.
+        receiveEmit(food){
+            alert("선택한 항목 : " + food);
+        }
+    }
+}
+
+</script>
+
+<style>
+    .wrapper{
+        display: flex;
+        flex-wrap:wrap;
+    }
+
+    .wrapper > div{
+        border:dashed black 1px;
+        flex-basis: 150px;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
+</style>
