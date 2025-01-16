3997dd240a4dc2f5a2a79b2713dae0ca221191db
jmcho2010 <projm2786@gmail.com>
2025-01-16 09:21:03 +0900

    test

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './app.vue' // app 컴포넌트 인식
+import App from './app-todoList.vue' // app 컴포넌트 인식
 //import ItemList from './components/ItemList.vue'//ItemList 정보 
 
 
 const app = createApp(App)
 
 //app.component('item-list', ItemList);
+app.component('props-exam', PropsExam)
+
 
 app.mount('#app')
