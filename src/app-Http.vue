a009015c194b58f789849b27c5538f768227ef2a
jmcho2010 <projm2786@gmail.com>
2025-01-17 10:10:56 +0900

    test

---
+++ src/app-Http.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button @click="fetchData">Fetch Data</button>
+        <p v-if="data">{{ data }}</p>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            data: null,
+        };
+    },
+    methods:{
+        fetchData(){
+            const response = fetch("http://jsonplaceholder.typicode.com/posts");
+            this.data = response;
+        }
+    }
+}
+
+
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@
 import { createApp } from 'vue'
 //import App from './app.vue' // app 컴포넌트 인식
 //import App from './appQuiz.vue';
-import App from './app-Teleport.vue'
-import tel from './teleport/Tel.vue'
+//import App from './app-Teleport.vue'
+import App from './app-Http.vue'
+//import tel from './teleport/Tel.vue'
 //import ItemList from './components/ItemList.vue'//ItemList 정보 
 //import PropsExam from './components/PropsExam.vue';
 // import SlotComp from './components/SlotComp.vue';
@@ -13,7 +14,7 @@ import tel from './teleport/Tel.vue'
 // import CompOne from './components/CompOne.vue'
 // import CompTwo from './components/CompTwo.vue'
 const app = createApp(App)
-app.component('tel', tel);
+//app.component('tel', tel);
 //app.component('slot-comp', SlotComp);
 
 //app.component('item-list', ItemList);
