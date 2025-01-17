b833fd4089669da15749f701d29900f794a9f68f
jmcho2010 <projm2786@gmail.com>
2025-01-17 10:35:26 +0900

    test

--- src/app-Http.vue
+++ src/app-Http.vue
@@ -42,4 +42,22 @@ export default{
     </div>
 </template>
 
+<script>
+    export default{
+        data(){
+            return{
+                posts: [],
+                loading: false,
+                error: null
+            }
+        },
+        async created(){
+            this.loading = true
+            const response = await axios.get("http://jsonplaceholder.typicode.com/posts");
+            this.posts = response.data
+            
+        }
+    }
+</script>
+
 <style></style>
