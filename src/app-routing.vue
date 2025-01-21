3ef7c40846611dce1a475ca55d9ed3a18d7b2f04
jmcho2010 <projm2786@gmail.com>
2025-01-21 09:44:00 +0900

    test

---
+++ src/app-routing.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>라우팅 메인파일임</p>
+    <router-link to="/animals">세계의 동물들</router-link><br>
+    <router-link to="/food">세계의 음식들</router-link>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 //import App from './app.vue' // app 컴포넌트 인식
 //import App from './appQuiz.vue';
 //import App from './app-Teleport.vue'
 //import App from './app-Http.vue'
-import App from './app-LifeCycle.vue'
+//import App from './app-LifeCycle.vue'
+import App from './app-routing.vue'
 //import tel from './teleport/Tel.vue'
 //import ItemList from './components/ItemList.vue'//ItemList 정보 
 //import PropsExam from './components/PropsExam.vue';
@@ -14,13 +16,28 @@ import App from './app-LifeCycle.vue'
 //import lifeCycle2 from './components/beforeMount.vue'
 //import lifeCycle3 from './components/beforeUpdate.vue'
 //import lifeCycle4 from './components/beforeDelete.vue'
-import lifeCycle5 from './components/render.vue'
+//import lifeCycle5 from './components/render.vue'
 // import CompOne from './components/CompOne.vue'
 // import CompTwo from './components/CompTwo.vue'
+import FoodItems from './route/FoodItems.vue'
+import Collections from './route/Collections.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/food', component: FoodItems},
+        {path:'/animals', component: Collections},
+    ]
+});
+
+
+
+
 const app = createApp(App)
+app.use(router);
 //app.component('comp-one', lifeCycle2)
 //app.component('comp-one', lifeCycle4)
-app.component('comp-one', lifeCycle5)
+//app.component('comp-one', lifeCycle5)
 //app.component('comp-one', lifeCycle3)
 //app.component('comp-one', lifeCycle1)
 //app.component('tel', tel);
