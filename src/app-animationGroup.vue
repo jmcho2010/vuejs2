e6644234a5dbd889a2e07b2908dc6a359dba157a
jmcho2010 <projm2786@gmail.com>
2025-01-22 10:17:55 +0900

    test

---
+++ src/app-animationGroup.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <input type="text" v-model="inpName">
+    <button @click="addEl">추가하기</button>
+    <TransitionGroup tag="ol">
+        <li v-for="i in products" :key="i">
+            {{ i }}
+        </li>
+    </TransitionGroup>
+
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                products: ['사과', '딸기', '키위'],
+                inpName: ''
+            }
+        },
+        methods:{
+            addEl(){
+                const el = this.inpName;
+                this.products.push(el);
+                this.inpName = null;
+            }
+        }
+    }
+</script>
+
+<style>
+  .v-enter-from {
+    opacity: 0;
+    rotate: 180deg;
+  }
+  .v-enter-to {
+    opacity: 1;
+    rotate: 0deg;
+  }
+  .v-enter-active {
+    transition: all 0.7s;
+  }
+</style>
